41dbdb2034d24585b4914c255bac76848f05c568
Ashaiky36 <ahadshaikh2436@gmail.com>
2026-01-28 23:39:16 +0530

    Final Project- day 4- user interface and user inputs

--- University Course Management System-Final Capstone Project/uni-cms.py
+++ University Course Management System-Final Capstone Project/uni-cms.py
@@ -27,18 +27,10 @@ class DataHandler:
              print("file does not exists")    
 
 
-# print("University Course Management System")
-# print("----------start screen-------------")
+print("University Course Management System")
+print("----------start screen-------------")
+print("enter your user id to login and access role specific features")
 
-# u_id = str(input("Enter your unique user id:"))
-
-# def verify_user(u_id, filepath = "users.json"):
-#     filepath = "users.json"
-#     with open(filepath, 'r') as file:
-#         data = json.load(file)
-#         for i in data:
-#             if i["id"] == u_id:
-#                 return True   
 
 user_id = str(input("Enter your unique user id:"))
                                 
@@ -53,8 +45,15 @@ class User:
 
         for user in users:
             if user["id"] == user_id:
-                print("user exists. login successful")
+                print("login successful")
                 if user["role"] == "admin":
+                    print("""
+admin features:
+create course = enter 1
+delete course = enter 2
+view all courses = enter 3
+exit = enter 4""")
+                   
                     class Admin(User):
                         # c_id = str(input("Enter a unique id for the course:"))
                         # c_name = str(input("Enter the course name:"))
@@ -95,11 +94,43 @@ class User:
                         def view_all_courses(self):
                             course_list = DataHandler.load_data("courses.json")
                             print(course_list)
-                    c1 = Admin()       
-                    c1.view_all_courses() 
+
+                    while True:
+                        
+                         admin_option = int(input("select feature:"))
+                         if admin_option == 1:
+                                c_id = str(input("course id:"))
+                                c_name = str(input("course name:"))        
+                                c_instructor = str(input("course's instructor name:"))
+                                c_capacity = int(input("maximum course capacity:"))
+                                acc_1 = Admin(c_id, c_name, c_instructor, c_capacity)       
+                                acc_1.create_course() 
+
+                         elif admin_option == 2:
+                                course_to_delete = str(input("course id of the course you want to delete:")) 
+                                adc_1 = Admin(course_to_delete)
+                                adc_1.delete_course()
+
+                         elif admin_option == 3:
+                                vac_1 = Admin()
+                                vac_1.view_all_courses()
+
+                         elif admin_option == 4:
+                             print("logging off......")
+                             print("exiting......")  
+                             break     
+
+                         else:
+                                print("enter a valid option")           
 
 
                 elif user["role"] == "student":
+                    print("""
+student features:
+view all courses = enter 1
+enroll in a course = enter 2
+view schedule = enter 3
+exit = enter 4""")
                     class Student(User):
                         def __init__(self, c_id = None):
                             self.c_id = c_id
@@ -138,10 +169,32 @@ class User:
                                 for course_id in schedule[user_id]:
                                     for course in schedule_details:
                                         if course["id"] == course_id:
-                                            print(f"{course["id"]}, {course["name"]} , {course["instructor"]}")       
+                                            print(f"{course["id"]}, {course["name"]} , {course["instructor"]}") 
 
-                    d1 = Student("MATH310")
-                    d1.enroll_in_course()
+                    while True:
+                        student_option = int(input("select feature:"))    
+                        if student_option == 1:
+                            vac_2 = Student()
+                            vac_2.view_all_courses()  
+
+                        elif student_option == 2:
+                            course_id = str(input("enter course id:"))    
+                            eic_1 = Student(course_id)       
+                            eic_1.enroll_in_course()
+
+                        elif student_option == 3:
+                            vs_1 = Student()
+                            vs_1.view_schedule()
+
+                        elif student_option == 4:
+                             print("logging off......")
+                             print("exiting......")  
+                             break     
+
+                        else:
+                                print("enter a valid option")                         
+
+                   
 
 
 
